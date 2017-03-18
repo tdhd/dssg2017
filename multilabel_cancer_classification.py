1b570a390b97c44d6275cbc97ba5c36f6476a738
Felix Biessmann <felix.biessmann@tu-berlin.de>
2017-03-18 15:19:19 +0100

    Some speedups and more features - parallelized one-vs-rest classifier - more cleaning for label tokenization

--- multilabel_cancer_classification.py
+++ multilabel_cancer_classification.py
@@ -7,39 +7,57 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score,precision_score,recall_score,f1_score
 from scipy.sparse import hstack
+import warnings,json,gzip
+
 
 def classify_cancer(fn = "/Users/felix/Data/master/features/features.csv"):
     '''
     Runs a multilabel classification experiment
     '''
-    X,y = getFeaturesAndLabels(fn)
+    X,y,labelNames = getFeaturesAndLabels(fn)
     # a train test split
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.8)
-    # train a classifier
-    classif = OneVsRestClassifier(SGDClassifier()).fit(X_train, y_train)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+    # turn off warnings, usually there are some labels missing in the training set
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        # train a classifier
+        print("Training classifier")
+        classif = OneVsRestClassifier(SGDClassifier(n_jobs=-1)).fit(X_train, y_train)
     # predict
     y_predicted = classif.predict(X_test)
     # the scores we want to compute
     scorers = [precision_score,recall_score,f1_score]
-    # compute Scores
-    metrics = {s.__name__:getSortedMetrics(y_test,y_predicted,labelVectorizer.classes_,s) for s in scorers}
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        # compute Scores
+        metrics = {s.__name__:getSortedMetrics(y_test,y_predicted,labelNames,s) for s in scorers}
     # dump results
-    json.dump(metrics,open("multilabel_classification_metrics.json","wb"))
+    json.dump(metrics,gzip.open("multilabel_classification_metrics.json","w"))
+    return metrics
 
 def getFeaturesAndLabels(fn):
     '''
     Load and vectorizer features and labels (vectorized using MultiLabelBinarizer)
     '''
+    print("Reading data")
     df = pd.read_csv(fn)
     # tokenize and binarize cancer classification labels
+    print("Vectorizing labels")
     labelVectorizer = MultiLabelBinarizer()
-    y = labelVectorizer.fit_transform(df.classifications.apply(tokenizeCancerLabels))
+    y = labelVectorizer.fit_transform(df.classifications.str.replace('[\[\]\'\"]',"").apply(tokenizeCancerLabels))
+    print("Vectorized %d labels"%y.shape[-1])
+    print("Vectorizing title character ngrams")
     titleVectorizer = HashingVectorizer(analyzer="char_wb",ngram_range=(1,4),n_features=2**12)
-    titleVects = titleVectorizer.fit_transform(df.fulltitle)
+    titleVects = titleVectorizer.fit_transform(df.fulltitle.fillna(""))
+    print("Vectorizing keywords")
     keywordVects = CountVectorizer().fit_transform(df.searchquery_terms.str.replace('[\[\]\'\"]',""))
-    authorVects = HashingVectorizer(n_features=2**12).fit_transform(df.author.str.replace('[\[\]\'\"]',""))
-    X = hstack((titleVects,keywordVects,authorVects))
-    return X,y
+    print("Vectorizing authors")
+    authorVects = HashingVectorizer(n_features=2**12).fit_transform(df.author.fillna("").str.replace('[\[\]\'\"]',""))
+    print("Vectorizing abstracts")
+    abstractVects = HashingVectorizer(n_features=2**12).fit_transform(df.abstract.fillna("").str.replace('[\[\]\'\"]',""))
+    X = hstack((titleVects,keywordVects,authorVects,abstractVects))
+    print("Extracted feature vectors with %d dimensions"%X.shape[-1])
+    return X,y,labelVectorizer.classes_
 
 def getSortedMetrics(true, predicted, labels, scorer):
     '''
@@ -53,4 +71,4 @@ def tokenizeCancerLabels(s):
     '''
     Tokenize the label string and remove empty strings
     '''
-    return [t for t in s.strip('[]\'').split(",") if len(t)>0]
+    return [t for t in s.split(",") if len(t)>0]
