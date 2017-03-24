b092345b3dcc68c7bbaf70b0bfb0a6d8ba0afcbe
Philipp Schmidt <tdhd@users.noreply.github.com>
2017-03-24 16:04:49 +0100

    parsing classifications into cancer, paper and sub classifications. add to classify cancer

--- labels.py
+++ labels.py
@@ -1,5 +1,21 @@
 
+def classifications_list_to_cancer_paper_subs(row):
+    '''
+    apply this function to the already parsed classifications list (output of labels.to_list)
 
+    df['classifications_list'] = df.apply(lambda row: to_list(row, 'classifications'), axis=1)
+    df['cancer_paper_subs'] = ['classifications_list'].apply(classifications_list_to_cancer_paper_subs)
+
+    this function converts each entry in the classification list
+    to separate cancer type(s), paper type and subcategories
+    '''
+    # split cancer types from papertype-subs
+    l = map(lambda e: e.split(","), row)
+    l = [item for sublist in l for item in sublist]
+    # split paper type from subs
+    l = map(lambda e: e.split("-"), l)
+    l = [item for sublist in l for item in sublist]
+    return l
 
 def to_list(row, column):
     '''
