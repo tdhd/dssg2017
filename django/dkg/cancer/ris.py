2fb26088454e5d49beb538685a45dfd5d83696c2
Felix Biessmann <felix.biessmann@tu-berlin.de>
2017-07-28 15:36:45 +0200

    Some export functionality for RIS file dumps. Assumes current Tuple format and column name 'keywords' for predicted kws Annotator name and score are discarded in RIS export. TODO: Change in inference article format

--- django/dkg/cancer/ris.py
+++ django/dkg/cancer/ris.py
@@ -3,6 +3,9 @@ import re
 RISSTART = '\r'
 
 def read_article(lines):
+    '''
+    Reads single RIS tag
+    '''
     article = {}
     for line in lines:
         match = re.match("[A-Z0-9]{2}\s+-",line)
@@ -14,7 +17,42 @@ def read_article(lines):
     return article
 
 def read_ris_lines(lines):
+    '''
+    RIS file import
+    INPUT:
+    lines   lines of RIS file
+    OUTPUT:
+    pandas dataframe, columns corresponding to RIS tags
+    '''
+
     import pandas as pd
     startArticle = [idx for idx,l in enumerate(lines) if re.match(RISSTART, l)]
     articles = [read_article(lines[startArticle[s]:startArticle[s+1]]) for s in range(len(startArticle)-1)]
     return pd.DataFrame(articles)
+
+def write_article(article):
+    '''
+    Writes single article to RIS string
+    the column 'keywords' is assumed to contain the assigned keywords as tuples
+    ('label','score','anotator')
+    TODO: better keyword-list flattening, currently only labels are extracted
+            problem is that there is only the KW RIS tag,
+            we'd need annotator/score specific RIS tags
+    '''
+    ris = []
+    for k,v in article.items():
+        if k == 'keywords':
+            for kw in v:
+                ris.append("KW  - " + kw[0])
+        else:
+            ris.append(k + "  - " + v)
+    return "\n".join(ris)
+
+def write_ris_lines(fn, df):
+    '''
+    Writes a pandas dataframe to RIS file.
+
+    '''
+    with open(fn, 'w') as fh:
+        for _, article in df.iterrows():
+            fh.write(write_article(article) + "\n\n")
