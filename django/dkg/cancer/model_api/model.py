ca9669e20bdfe2f2cacc764996230e303e410e4a
Philipp Schmidt <tdhd@users.noreply.github.com>
2017-07-30 21:39:57 +0200

    encode publisher as-is and publishing year with hashing-vectorizer

--- django/dkg/cancer/model_api/model.py
+++ django/dkg/cancer/model_api/model.py
@@ -27,10 +27,17 @@ def encode_features_of(articles_with_keywords_and_probas):
     JF = sklearn.feature_extraction.text.HashingVectorizer(n_features=2**8)
     X_JF = JF.fit_transform(articles_with_keywords_and_probas.JF)
 
-    PB = sklearn.feature_extraction.text.HashingVectorizer(n_features=2**8)
+    PB = sklearn.preprocessing.LabelBinarizer(sparse_output=True)
     X_PB = PB.fit_transform(articles_with_keywords_and_probas.PB)
+    # PB = sklearn.feature_extraction.text.HashingVectorizer(n_features=2**8)
+    # X_PB = PB.fit_transform(articles_with_keywords_and_probas.PB)
 
-    X = scipy.sparse.hstack((X_T1, X_N2, X_JA, X_JF, X_PB))
+    # Y1 = sklearn.preprocessing.LabelBinarizer(sparse_output=True)
+    # X_Y1 = Y1.fit_transform(articles_with_keywords_and_probas.Y1)
+    Y1 = sklearn.feature_extraction.text.HashingVectorizer(n_features=2**8)
+    X_Y1 = Y1.fit_transform(articles_with_keywords_and_probas.PB)
+
+    X = scipy.sparse.hstack((X_T1, X_N2, X_JA, X_JF, X_PB, X_Y1))
 
     print 'encoded features shape {}'.format(X.shape)
 
