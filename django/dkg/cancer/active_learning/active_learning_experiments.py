54aaa87be867aa7084e676ba23badb0352c9f78f
Felix Biessmann <felix.biessmann@tu-berlin.de>
2017-10-26 19:11:32 +0200

    Minor cleanups and several speedups for training     - Moved active learning code into active_learning subdir     - Saving outer CV, reporting cv average score     - Using L2 regularizer     - Using charngrams again for authors (seems to work better)     - Added notification if no keywords were predicted, to avoid impression of something didn't work

--- django/dkg/cancer/active_learning/active_learning_experiments.py
+++ django/dkg/cancer/active_learning/active_learning_experiments.py
@@ -71,6 +71,55 @@ def run_experiment(test_size=0.6, n_reps=5, percentage_samples=[1,2,5,10,15,30,5
     json.dump(results,open("active_learning_curves.json","wt"))
     return active_learning_curves, random_learning_curves, baseline_lows, baseline_highs
 
+def compute_active_learning_curve(X_train,y_train,X_test,y_test,X_validation, y_validation, clf,percentage_samples=[1,2,5,10,15,30,50,100]):
+    '''
+    Emulate active learning with annotators:
+    for a given training, test and validation set, get the validation error by
+    training on training data only, then the score when trained on training and
+    test data and then the increasing validation score when adding more labelled
+    data, either with random selection or with active learning. The results are
+    the increase in scores with the respective sampling policy
+    '''
+    print('Computing active learning curve:')
+    clf = OneVsRestClassifier(SGDClassifier(loss="log",alpha=clf.estimator.alpha, average=True, penalty='l1'), n_jobs=-1).fit(X_train, y_train)
+    baseline_low = label_ranking_average_precision_score(y_validation.toarray(), clf.predict_proba(X_validation))
+    clf_trained = OneVsRestClassifier(SGDClassifier(loss="log",alpha=clf.estimator.alpha, average=True, penalty='l1'), n_jobs=-1).fit(vstack([X_train, X_test]), vstack([y_train, y_test]))
+    baseline_high = label_ranking_average_precision_score(y_validation.toarray(), clf_trained.predict_proba(X_validation))
+    print('\tBaseline on test: {}, baseline score on train and test {}'.format(baseline_low, baseline_high))
+
+    # score test data for active learning sorting
+    label_probas = clf.predict_proba(X_test)
+
+    # run a random sampling procedure for training with increasing amounts of labels
+    random_priorities = np.random.permutation(label_probas.shape[0])
+
+    random_learning_curve = []
+    for percentage in percentage_samples:
+        n_samples = int((percentage/100.) * X_test.shape[0])
+        X_labelled = X_test[random_priorities[:n_samples],:]
+        y_labelled = y_test[random_priorities[:n_samples],:]
+        clf_current = OneVsRestClassifier(SGDClassifier(loss="log",alpha=clf.estimator.alpha, average=True, penalty='l1'), n_jobs=-1).fit(vstack([X_train, X_labelled]), vstack([y_train, y_labelled]))
+        current_score = label_ranking_average_precision_score(y_validation.toarray(), clf_current.predict_proba(X_validation))
+        print('\t(RANDOM) Trained on {} samples ({}%) from test set - reached {} ({}%)'.format(n_samples, percentage, current_score, np.round(100.0*(current_score - baseline_low)/(baseline_high-baseline_low))))
+        random_learning_curve.append(current_score)
+
+    # mean distance to hyperplane
+    dists = abs(logit(label_probas)).mean(axis=1)
+    # run active learning procedure for training with increasing amounts of labels
+    priorities = dists.argsort()
+
+    active_learning_curve = []
+    for percentage in percentage_samples:
+        n_samples = int((percentage/100.) * X_test.shape[0])
+        X_labelled = X_test[priorities[:n_samples],:]
+        y_labelled = y_test[priorities[:n_samples],:]
+        clf_current = OneVsRestClassifier(SGDClassifier(loss="log",alpha=clf.estimator.alpha, average=True, penalty='l1'),n_jobs=-1).fit(vstack([X_train, X_labelled]), vstack([y_train, y_labelled]))
+        current_score = label_ranking_average_precision_score(y_validation.toarray(), clf_current.predict_proba(X_validation))
+        print('\t(ACTIVE LEARNING) Trained on {} samples ({}%) from test set - reached {} ({}%)'.format(n_samples, percentage, current_score, np.round(100.0*(current_score - baseline_low)/(baseline_high-baseline_low))))
+        active_learning_curve.append(current_score)
+
+    return active_learning_curve, random_learning_curve, baseline_low, baseline_high
+
 def plot_results(fn):
     import pylab
     results = json.load(open(fn))
